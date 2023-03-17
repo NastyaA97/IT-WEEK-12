9d53e522c7f8d109420b95d16c1374d4adebf384
NastyaA97 <nastyaabramovich2011@mail.ru>
2023-03-17 16:05:39 +0300

    new file name

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function capitalize(str) {
     return str.replace(/(^|\s)\S/g, (a) => {return a.toUpperCase()})
 }
 
-function SEND(){
+function send(){
 
     chatName.innerText=capitalize(name.value.toLowerCase());
     chatImage.src=image.value;
